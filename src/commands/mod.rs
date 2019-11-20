4d112ce235ae5d5976a43eb281bebf4b8bae5983
Hüsen Baghayi <hossein.baghayi@gmail.com>
2019-11-21 03:04:01 +0330

    undefined command error

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,4 +1,10 @@
 use std::collections::HashMap;
+use std::cmp::PartialEq;
+
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    UndefinedCommand,
+}
 
 pub trait Command {
     fn name(&self) -> &'static str;
@@ -21,11 +27,14 @@ impl<T: Command> Commands<T> {
         self.list.insert(command.name(), command);
     }
 
-    pub fn run(&mut self, command_name: &str) {
+    pub fn run(&mut self, command_name: &str) -> Result<(), Error> {
         let result = self.list.get_mut(command_name);
-        match result {
-            Some(cmd) => cmd.run(),
-            None => unimplemented!(),
+        return match result {
+            Some(cmd) => {
+                cmd.run();
+                Ok(())
+            },
+            None => Err(Error::UndefinedCommand),
         }
     }
 }
@@ -60,7 +69,20 @@ mod tests {
         let command = ACommand{buffer: &mut buffer};
         let mut commands = Commands::new();
         commands.register(command);
-        commands.run("command-name");
+        let _ = commands.run("command-name");
         assert_eq!(buffer, "ran command");
+    }
+
+    #[test]
+    fn not_found_command_error_when_trying_to_run_an_undefined_command() {
+        let mut buffer = String::new();
+        let command = ACommand{buffer: &mut buffer};
+        let mut commands = Commands::new();
+        commands.register(command);
+        let result = commands.run("undefined");
+        match result {
+            Err(e) => assert_eq!(Error::UndefinedCommand, e),
+            Ok(_) => panic!("cannot run undefined command"),
+        }
     }
 }
